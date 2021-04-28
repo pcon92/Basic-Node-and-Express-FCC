6116dcbf3ba2526d27b85d6d9122ad3bf630819b
Paul C <paul_congiu@hotmail.com>
2021-04-28 12:57:18 +1000

    used to uppercase

--- myApp.js
+++ myApp.js
@@ -21,8 +21,8 @@ app.get("/", (req, res) => {
 */
 
 app.get("/json", (req, res) => {
-    if (process.env.MESSAGE_STYLE === 'uppercase') {
-        res.json({"message":"HELLO JSON"})
+    if (process.env.MESSAGE_STYLE) {
+        res.json({"message": "Hello json"}).toUpperCase();
     } else {
         res.json({"message": "Hello json"})
     }
