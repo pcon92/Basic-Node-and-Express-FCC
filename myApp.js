a092b1bbdfb1840f8c0475f663f9f1f5f0b02cca
Paul C <paul_congiu@hotmail.com>
2021-04-28 12:53:54 +1000

    using variable from .env file for FCC challenge

--- myApp.js
+++ myApp.js
@@ -21,7 +21,12 @@ app.get("/", (req, res) => {
 */
 
 app.get("/json", (req, res) => {
-    res.json({"message": "Hello json"})
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({"message":"HELLO JSON"})
+    } else {
+        res.json({"message": "Hello json"})
+    }
+
 });
 
 
