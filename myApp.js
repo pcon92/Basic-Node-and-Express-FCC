945075ca911fb4403d8cbb7181dc4c1c421b19cf
Paul C <paul_congiu@hotmail.com>
2021-04-28 12:39:30 +1000

    serve static assets using express static

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
+
+/* Challenge 1
 console.log("Hello World");
+*/
 
 /* Challenge 2
 app.get("/", (req, res) => {
@@ -9,11 +13,13 @@ app.get("/", (req, res) => {
 })
 */
 
-/* Challenge 3 */
+/* Challenge 3 
 app.get("/", (req, res) => {
     const absolutePath = __dirname + "/views/index.html";
     res.sendFile(absolutePath);
 });
+*/
+
 
 
 
