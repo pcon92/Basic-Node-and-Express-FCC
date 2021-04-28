5383380d5f70e2bb8173011a7e53002f89624f68
Paul C <paul_congiu@hotmail.com>
2021-04-28 12:29:52 +1000

    serve HTML file for FCC challenge

--- myApp.js
+++ myApp.js
@@ -3,11 +3,17 @@ var app = express();
 
 console.log("Hello World");
 
+/* Challenge 2
 app.get("/", (req, res) => {
     res.send("Hello Express");
 })
+*/
 
-
+/* Challenge 3 */
+app.get("/", (req, res) => {
+    const absolutePath = __dirname + "/views/index.html";
+    res.send(absolutePath);
+});
 
 
 
