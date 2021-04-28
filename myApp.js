32214d25c77ea5304d2f18eec99b2b63efd3cb33
Paul C <paul_congiu@hotmail.com>
2021-04-28 13:52:40 +1000

    added dash as requested by FCC challenge

--- myApp.js
+++ myApp.js
@@ -4,9 +4,8 @@ require('dotenv').config();
 
 app.use("/public", express.static(__dirname + "/public"));
 
-
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path} ${req.ip}`)
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
     next();
 });
 
